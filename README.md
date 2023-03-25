f1d813f1d5839adbaaefdf296c858fe9aaaa6a8b
somenoe <otashi24@gmail.com>
2023-03-25 23:17:20 +0700

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Show image
+
+Dummy file index server using path matching patterns and `res.download()` to
+serve files to the user.
+and show image with `res.send("<img src=...>")`.
+
+## How to run this example
+
+Run this example using:
+
+```bash
+deno run --allow-net --allow-read index.ts
+```
+
+## clone from 
+
+https://github.com/cmorten/opine
+
+## original folder from
+
+https://github.com/cmorten/opine/tree/main/examples/downloads
---
+++ index.ts
@@ -0,0 +1,77 @@
+/**
+ * Run this example using:
+ *
+ *    deno run --allow-net --allow-read ./examples/downloads/index.ts
+ *
+ *    after cloning the repo locally.
+ */
+
+import { dirname, join } from "https://deno.land/x/opine@2.3.3/deps.ts";
+import opine from "https://deno.land/x/opine@2.3.3/mod.ts";
+
+const app = opine();
+const __dirname = dirname(import.meta.url);
+
+app.get("/", function (_req, res) {
+  res.send(
+    "<ul>" +
+      '<li>Download <a href="/files/amazing.txt">amazing.txt</a>.</li>' +
+      '<li>Download <a href="/files/missing.txt">missing.txt</a>.</li>' +
+      '<li>Download <a href="/files/CCTV大赛上海分赛区.txt">CCTV大赛上海分赛区.txt</a>.</li>' +
+      "</ul>",
+  );
+});
+
+// If we used `/files/*`, the name could be accessed via req.params[0]
+// but here we have named it using :file
+app.get("/files/:file(*)", async function (req, res, next) {
+  const filePath = join(__dirname, "files", req.params.file);
+
+  try {
+    await res.download(filePath);
+  } catch (err) {
+    // file for download not found
+    if (err instanceof Deno.errors.NotFound) {
+      res.status = 404
+      res.send("Cant find that file, sorry!?");
+
+      return;
+    }
+
+    // non-404 error
+    return next(err);
+  }
+});
+
+const DEV_URL="http://localhost:3000/"
+
+app.get("/show/files/:file(*)", async function (req, res, next) {
+  const fileName=req.params.file
+
+  try {
+    await res.send(`<img src=${DEV_URL+'files/'+ fileName} alt=${fileName} width="300">`);
+  } catch (err) {
+    // file for download not found
+    if (err instanceof Deno.errors.NotFound) {
+      res.status = 404
+      res.send("Cant find that file, sorry!?");
+
+      return;
+    }
+
+    // non-404 error
+    return next(err);
+  }
+});
+
+if (import.meta.main) {
+  // You can call listen the same as Express with just
+  // a port: `app.listen(3000)`, or with any arguments
+  // that the Deno `http.serve` methods accept. Namely
+  // an address string, HttpOptions or HttpsOptions
+  // objects.
+  app.listen({ port: 3000 });
+  console.log("Opine started on port 3000");
+}
+
+export { app };
